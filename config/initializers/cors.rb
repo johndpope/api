cdc724ad6a3773bab91e5de0955dc3d7239d23b9
Julich Mera <mera7617@gmail.com>
2016-05-04 22:47:56 -0400

    Update CORS policy Things break during app initialization if CORS origins are nil.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 # For now this will do
 js_origins = {
-  'development' => 'localhost:4200'
+  'development' => 'localhost:4200',
+  'production' => 'rentirooms.com',
+  'staging' => 'staging.rentirooms.com'
 }
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
