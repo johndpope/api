ab6f1c00878a934249f93d7bae7248e81b396eda
Julich Mera <mera7617@gmail.com>
2016-05-06 08:50:01 -0400

    ActiveModel::Serializer::Adapter::JsonApi is deprecated Use ActiveModelSerializers::Adapter::JsonApi

--- config/initializers/ams_json_adapter.rb
+++ config/initializers/ams_json_adapter.rb
@@ -1,2 +1,2 @@
-# ActiveModel Serializer JSON Adapter
-ActiveModel::Serializer.config.adapter = ActiveModel::Serializer::Adapter::JsonApi
+# ActiveModel Serializer JSON Adapter is what Ember expects on the front end
+ActiveModel::Serializer.config.adapter = ActiveModelSerializers::Adapter::JsonApi
