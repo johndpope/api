65c1721799d07ec8a8764c949b58abf059cccf20
Julich Mera <mera7617@gmail.com>
2016-05-11 17:29:25 -0400

    Rails expects index.html string to be HTML safe

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,10 @@
 class IndexController < ApplicationController
   def index
-    render(html: $redis.get('client:index:current-content') || 'No Content')
+    render(html: current_index.html_safe)
   end
+
+  private
+    def current_index
+      $redis.get('client:index:current-content') || 'No content'
+    end
 end
