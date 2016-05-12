dac0ea196b9859c17c39d72bd4c85322d6119f52
Julich Mera <mera7617@gmail.com>
2016-05-12 00:12:09 -0400

    Redirect frontend routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   resources :users
   mount Resque::Server => '/resque'
+
+  # Looks like this is a frontend route (e.g. i-have-a-room, how-it-works)
+  # If it's not xhr redirect to / and pass url as param
+  # Ignore xhr because frontend should know how backend routes
+  # For security purposes only route GET requests
+  not_xhr = Class.new do
+    def matches?(req) !req.xhr? end
+  end
+
+  get '*url', to: redirect { |path_params, req|
+    "?#{req.params.to_query}"
+  }, constraints: not_xhr.new
 end
